70baf24994b0f9a23e046159d7157cc9cfbb5df4
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2023-10-09 21:44:07 +0200

    variables

--- first_steps/src/main.rs
+++ first_steps/src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    // by default variabels are not changeable 
+    // let x = 4; // implicitly assigned
+    // let x: u32 = 4; assigned with type
+
+    /*
+
+    let mut x = 4;
+
+    println!("x is: {}", x);
+    x = 5;
+    println!("x is: {}", x);
+
+    */
+
+    /*
+    
+    let x = 4;
+    println!("x is: {}", x);
+
+    { // new scope
+        let x = x - 2; // x can be used from exterior scope
+        println!("x is: {}", x);
+    }
+
+    let x = "hello";
+    println!("x is: {}", x);
+
+    */
+    
+    const SECONDS_IN_MINUTES: u32 = 10; // constants MUST have a type | can only be defined ONCE
+    println!("{}", SECONDS_IN_MINUTES);
 }
